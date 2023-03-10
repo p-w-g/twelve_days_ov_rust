7e653682f4a74b7a7073a6c309d36ad1da32b57a
p-w-g <p.grzelczyk@gmail.com>
2023-03-10 18:22:42 +0100

    whip up some errors

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ fn main() {
         "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "nineth",
         "tenth", "eleventh", "twelve",
     ];
+    const ERRORS: [&str; 12] = [
+        "Using Sentinel Values.",
+        "Hungarian Notation.",
+        "An Abundance of Rc<RefCell<T>>",
+        "Using the Wrong Integer Type.",
+        "Unsafe - I Know What I'm Doing.",
+        "Not Using Namespaces.",
+        "Overusing Slice Indexing.",
+        "Overusing Iterators.",
+        "Not Leveraging Pattern Matching.",
+        "Initialize After Construction.",
+        "Defensive Copies.",
+        "Conclusions ",
+    ];
 
     for day in DAYS {
         println!("For the {day} day, my rust compiler gave to me:");
