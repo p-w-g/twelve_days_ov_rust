b14630202e5a7d4f6eae464d34a44c29ebbeff14
p-w-g <p.grzelczyk@gmail.com>
2023-03-10 18:16:59 +0100

    write main sentence

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    const DAYS: [&str; 12] = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "nineth",
+        "tenth", "eleventh", "twelve",
+    ];
+
+    for day in DAYS {
+        println!("For the {day} day, my rust compiler gave to me:");
+    }
 }
